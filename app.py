5e39aa206f971540e1e5af5fd59d1e45012d5920
impavan007 <raopavansingh007@gmail.com>
2025-05-31 19:02:32 +0530

    Added app, model, requirements, and .gitignore

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,request,jsonify
+import pickle
+import numpy as np 
+
+app=Flask(__name__)
+
+with open('model.pkl','rb') as f:
+    model=pickle.load(f)
+
+@app.route('/')
+def home():
+    return 'ml model is live'
+@app.route('/predict',methods=['POST'])
+def predict():
+    try:
+        data=request.get_json(force=True)
+        features=np.array(data['features']).reshape(1,-1)
+        print(features)
+        prediction=model.predict(features)
+        value='legitimate'
+        if (int(prediction[0])==1):
+            value='phishing'
+        return jsonify({'prediction ':value})
+    except Exception as e:
+        return jsonify({'error:':str(e)})
+    
+if __name__=='__main__':
+    app.run(debug=True)
+else:
+    print("not found")
